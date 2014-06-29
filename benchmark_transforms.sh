620b7378f19f5fd98ac0d94d69c5c1585552acdf
Dan Collis-Puro <djcp@thoughtbot.com>
2014-06-29 18:10:49 -0400

    Initial commit

---
+++ benchmark_transforms.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+PATH="./bin/:$PATH"
+
+benchmark_command(){
+  tool_to_benchmark=$@
+  echo "Benchmarking $tool_to_benchmark"
+  date
+  (find images -type f -name '*.jpg' -print0 | parallel -0 -I '{}' $tool_to_benchmark) 2> /dev/null
+  date
+}
+
+rm -f converted_ffmpeg_images/*
+FFMPEG="ffmpeg -i {} -vf scale=640:-1 converted_ffmpeg_{}"
+benchmark_command $FFMPEG
+
+rm -f converted_imagemagick_images/*
+IMAGEMAGICK="convert {} -resize 640 converted_imagemagick_{}"
+benchmark_command $IMAGEMAGICK
