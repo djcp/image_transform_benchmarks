b5362295437d32027caec34f34a37b95188470dc
Daniel Puro <djcp@Daniels-MacBook-Pro.local>
2014-07-01 13:12:07 -0400

    Add sips benchmarking

--- benchmark_transforms.sh
+++ benchmark_transforms.sh
@@ -4,15 +4,23 @@ PATH="./bin/:$PATH"
 benchmark_command(){
   tool_to_benchmark=$@
   echo "Benchmarking $tool_to_benchmark"
-  date
-  (find images -type f -name '*.jpg' -print0 | parallel -0 -I '{}' $tool_to_benchmark) 2> /dev/null
-  date
+  (find images -type f -name '*.jpg' -print0 | parallel -0 -I '{}' $tool_to_benchmark) &> /dev/null
 }
 
 rm -f converted_ffmpeg_images/*
-FFMPEG="ffmpeg -i {} -vf 'scale=640:-1 [in], movie=watermark.png [watermark]; [in][watermark] overlay=main_w-overlay_w-10:main_h-overlay_h-10 [out]' converted_ffmpeg_{}"
-benchmark_command $FFMPEG
+#FFMPEG="ffmpeg -i {} -vf 'scale=640:-1 [in], movie=watermark.png [watermark]; [in][watermark] overlay=main_w-overlay_w-10:main_h-overlay_h-10 [out]' converted_ffmpeg_{}"
+FFMPEG="ffmpeg -i {} -vf 'scale=640:-1' converted_ffmpeg_{}"
+time benchmark_command $FFMPEG
+
+rm -f converted_ffmpeg_images/*
+#FFMPEG="ffmpeg -i {} -vf 'scale=640:-1 [in], movie=watermark.png [watermark]; [in][watermark] overlay=main_w-overlay_w-10:main_h-overlay_h-10 [out]' converted_ffmpeg_{}"
+FFMPEG="ffmpeg -i {} -vf 'scale=640:-1' converted_ffmpeg_{}"
+time benchmark_command $FFMPEG
 
 rm -f converted_imagemagick_images/*
 IMAGEMAGICK="convert {} -resize 640 converted_imagemagick_{}"
-benchmark_command $IMAGEMAGICK
+time benchmark_command $IMAGEMAGICK
+
+rm -f converted_sips_images/*
+SIPS="sips -Z 640 {} --out converted_sips_{}"
+time benchmark_command $SIPS
